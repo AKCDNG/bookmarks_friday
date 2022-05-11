b51a47c974c5d5b53fb3fe8b5dcdcf826208d06e
Anish <anishkakaiya@outlook.com>
2022-05-11 17:48:48 +0100

    update unit test

--- spec/units/list_spec.rb
+++ spec/units/list_spec.rb
@@ -3,15 +3,15 @@ require 'list'
 describe List do
   before(:each) {
     con = PG.connect :dbname => 'bookmark_manager_test'
-    con.exec "INSERT INTO bookmarks(url) VALUES ('testurl1'), ('testurl2');"
+    con.exec "INSERT INTO bookmarks(url, title) VALUES ('http://testurl1.pl', 'Testurl1'), ('http://testurl2.es', 'Testurl2');"
    }
-  it 'should return Ask Jeeves' do
-    expect(List.all).to include ('testurl1')
-    expect(List.all).to include ('testurl2')
+  it 'should return testurls' do
+    expect(List.all).to include ('Testurl1')
+    expect(List.all).to include ('Testurl2')
   end
 
   it 'should return new added bookmark' do
-    List.add('testurl3')
-    expect(List.all).to include ('testurl3')
+    List.add('http://testurl3.org','Testurl3')
+    expect(List.all).to include ('Testurl3')
   end
 end
