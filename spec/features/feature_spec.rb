d4bfe2485cfc76fc3d87feb883a80cbe8982dd08
sjtinsley <simonjtinsley@gmail.com>
2022-05-12 00:57:33 +0100

    Small refactor of feature tests

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,17 +4,18 @@ require_relative "../../app"
 Capybara.app = BookmarkManager
 
 feature 'bookmarks' do
-  before(:each) {
-    con = PG.connect :dbname => 'bookmark_manager_test'
-    con.exec "INSERT INTO bookmarks(url, title) VALUES ('http://testurl1.pl', 'Testurl1'), ('http://testurl2.es', 'Testurl2');"
-   }
-  scenario 'view bookmarks' do
+  before(:all) {
+    empty_test_db
+    insert_2_url
+  }
+
+  scenario 'user can view bookmarks' do
     visit '/bookmarks'
     expect(page).to have_content "Testurl1"
     expect(page).to have_content "Testurl2"
   end
 
-  scenario 'add a bookmark' do
+  scenario 'user can add a bookmark' do
     visit '/bookmarks'
     fill_in "add_url", with: "http://testurl3.org"
     fill_in "add_title", with: "Testurl3"
--- spec/units/list_spec.rb
+++ spec/units/list_spec.rb
@@ -4,16 +4,21 @@ describe List do
   before(:each) { empty_test_db }
   before(:each) { insert_2_url }
   describe '#all' do
+    
     it 'should return testurls' do
       expect(List.all[0]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl1')
       expect(List.all[1]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl2')
     end
+
   end
+  
   describe '#add' do
     before(:example) { List.add('http://testurl3.org','Testurl3') }
+    
     it 'should return new added bookmark' do
       p List.all
       expect(List.all[2]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl3')
     end
+    
   end
 end
