bf5171a89820bf35db925b77f10d8214c0eb78c8
sjtinsley <simonjtinsley@gmail.com>
2022-05-12 00:54:12 +0100

    Refactored code, and completed tests for Bookmark class

--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -1,8 +1,13 @@
 require "bookmark.rb"
 
-describe "bookmark wrapping" do
+describe Bookmark do
+  let(:subject) {Bookmark.new("http://testurl.com", "Testurl")}
 
   it "returns title of website" do
-    expect(subject.title).to eq "Testurl3"
+    expect(subject.title).to eq "Testurl"
+  end
+
+  it "returns title of website" do
+    expect(subject.url).to eq "http://testurl.com"
   end
 end
--- spec/units/list_spec.rb
+++ spec/units/list_spec.rb
@@ -1,17 +1,19 @@
 require 'list'
 
 describe List do
-  before(:each) {
-    con = PG.connect :dbname => 'bookmark_manager_test'
-    con.exec "INSERT INTO bookmarks(url, title) VALUES ('http://testurl1.pl', 'Testurl1'), ('http://testurl2.es', 'Testurl2');"
-   }
-  it 'should return testurls' do
-    expect(List.all).to include ('Testurl1')
-    expect(List.all).to include ('Testurl2')
+  before(:each) { empty_test_db }
+  before(:each) { insert_2_url }
+  describe '#all' do
+    it 'should return testurls' do
+      expect(List.all[0]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl1')
+      expect(List.all[1]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl2')
+    end
   end
-
-  it 'should return new added bookmark' do
-    List.add('http://testurl3.org','Testurl3')
-    expect(List.all).to include ('Testurl3')
+  describe '#add' do
+    before(:example) { List.add('http://testurl3.org','Testurl3') }
+    it 'should return new added bookmark' do
+      p List.all
+      expect(List.all[2]).to be_instance_of(Bookmark).and have_attributes(:title => 'Testurl3')
+    end
   end
 end
