5ffb7622d5df829184214f8eb1bc9fd4f20143d3
Anish <anishkakaiya@outlook.com>
2022-05-11 16:14:46 +0100

    add self.add method

--- lib/list.rb
+++ lib/list.rb
@@ -7,20 +7,20 @@ class List
     else
       con = PG.connect :dbname => 'bookmark_manager'
     end
-      
       rs = con.exec "SELECT url FROM bookmarks;"
   
       rs.map do |row|
         row['url']
       end
-
-      
-
   end
 
-  
+  def self.add(new_bookmark)
+    if ENV['DB_ENV'] == 'test'
+      con = PG.connect :dbname => 'bookmark_manager_test'
+    else
+      con = PG.connect :dbname => 'bookmark_manager'
+    end
+    con.exec "INSERT INTO bookmarks (url) VALUES ('#{new_bookmark}');"
+  end
+
 end
-
-
-
-
