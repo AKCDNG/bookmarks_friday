8a4d1cf9c3f0f6d0647f4a4df8f0939ac498aa99
Anish <anishkakaiya@outlook.com>
2022-05-11 17:48:40 +0100

    update feature test

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -6,19 +6,19 @@ Capybara.app = BookmarkManager
 feature 'bookmarks' do
   before(:each) {
     con = PG.connect :dbname => 'bookmark_manager_test'
-    con.exec "INSERT INTO bookmarks(url) VALUES ('testurl1'), ('testurl2');"
+    con.exec "INSERT INTO bookmarks(url, title) VALUES ('http://testurl1.pl', 'Testurl1'), ('http://testurl2.es', 'Testurl2');"
    }
-
   scenario 'view bookmarks' do
     visit '/bookmarks'
-    expect(page).to have_content "testurl1"
-    expect(page).to have_content "testurl2"
+    expect(page).to have_content "Testurl1"
+    expect(page).to have_content "Testurl2"
   end
 
   scenario 'add a bookmark' do
     visit '/bookmarks'
-    fill_in "add_bookmark", with: "testurl3"
+    fill_in "add_url", with: "http://testurl3.org"
+    fill_in "add_title", with: "Testurl3"
     click_button 'Submit'
-    expect(page).to have_content "testurl3"
+    expect(page).to have_content "Testurl3"
   end
 end
