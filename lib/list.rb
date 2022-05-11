f4065fd8138e3900fd6f4ca5222eff3180cac97b
Anish <anishkakaiya@outlook.com>
2022-05-11 17:48:17 +0100

    wrap database in class

--- lib/list.rb
+++ lib/list.rb
@@ -1,26 +1,29 @@
 require 'pg'
+require_relative './bookmark'
 class List
 
   def self.all 
+    bookmarks = []
     if ENV['DB_ENV'] == 'test'
       con = PG.connect :dbname => 'bookmark_manager_test'
     else
       con = PG.connect :dbname => 'bookmark_manager'
     end
-      rs = con.exec "SELECT url FROM bookmarks;"
+      rs = con.exec "SELECT * FROM bookmarks;"
   
-      rs.map do |row|
-        row['url']
+      rs.each do |row|
+        bookmarks << Bookmark.new(row['url'], row['title'])
       end
+      bookmarks
   end
 
-  def self.add(new_bookmark)
+  def self.add(url, title)
     if ENV['DB_ENV'] == 'test'
       con = PG.connect :dbname => 'bookmark_manager_test'
     else
       con = PG.connect :dbname => 'bookmark_manager'
     end
-    con.exec "INSERT INTO bookmarks (url) VALUES ('#{new_bookmark}');"
+    con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}');"
   end
 
 end
